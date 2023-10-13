e48b0cd34f1f7ed96464e6fa487169f0e3f1e8d1
Krig Raseri <krigraseri@gmail.com>
2023-10-12 19:14:31 -0500

    init

---
+++ src/main/java/com/umbrella/music_advisor/MusicAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.umbrella.music_advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicAdvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umbrella/music_advisor/MusicAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrella.music_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
