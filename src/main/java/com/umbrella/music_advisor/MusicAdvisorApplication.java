088cd546d7de83a61d7eff5961b718fdbd64d336
Krig Raseri <krigraseri@gmail.com>
2023-10-25 19:11:31 -0500

    Update MusicAdvisorApplication.java

--- src/main/java/com/umbrella/music_advisor/MusicAdvisorApplication.java
+++ src/main/java/com/umbrella/music_advisor/MusicAdvisorApplication.java
@@ -2,12 +2,13 @@ package com.umbrella.music_advisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class MusicAdvisorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicAdvisorApplication.class, args);
     }
-
 }
